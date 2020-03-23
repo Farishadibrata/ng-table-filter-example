4b378e57486e06f4edb6798efaaf7042896a330f
Faris Hadibrata <farisraihanhadibrata@gmail.com>
2020-03-23 12:20:55 +0700

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { from, combineLatest, of } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'table-test';
+  observableData$  = of(require('./MOCK_DATA.json') )
+  originalData : any
+  filteredData$ : any
+  ngOnInit(){
+    this.observableData$.subscribe(data => {
+      this.originalData = data
+    })
+  }
+  filterData(string){
+    let val = string.toLowerCase()
+    this.filteredData$ = this.originalData.filter(function(d){
+      return d.first_name.toLowerCase().indexOf(val) !== -1 || !val
+    })
+  }
 }
